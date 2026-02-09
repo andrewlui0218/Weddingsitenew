d1ea4f8600b110eddecea70c24eca0cd933005df
andrewlui0218 <andrewlui0218@gmail.com>
2026-02-09 19:51:34 +0800

    Add files via upload

--- App.tsx
+++ App.tsx
@@ -9,8 +9,21 @@ import Rsvp from './components/Rsvp';
 import Footer from './components/Footer';
 
 const App: React.FC = () => {
+  const bgImage = "images/hero-bg.jpg";
+
   return (
-    <div className="w-full min-h-screen flex flex-col">
+    <div className="w-full min-h-screen flex flex-col relative">
+      {/* 
+        Global Background Layer 
+        - Fixed position so it stays while scrolling.
+        - z-[-1] ensures it stays behind content.
+        - It will show through the semi-transparent backgrounds of the sections.
+      */}
+      <div 
+        className="fixed inset-0 z-[-1] bg-cover bg-center bg-no-repeat"
+        style={{ backgroundImage: `url(${bgImage})` }}
+      ></div>
+
       <Hero />
       <EventDetails />
       <Navbar />
